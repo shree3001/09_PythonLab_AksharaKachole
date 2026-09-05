ef92fee2595ade47b2a02576066ffd1d0517fd89
Shree sharma <akachole469@gmail.com>
2026-09-05 10:39:32 +0530

    Add files via upload

---
+++ lab2/Rhombus.py
@@ -0,0 +1,8 @@
+#Rhombus pattern
+n = 5
+
+for i in range(n):
+    print(" " * (n-i-1) + "* " * (i+1))
+
+for i in range(n-1, 0, -1):
+    print(" " * (n-i) + "* " * i)
--- lab2/Triangle.py
+++ lab2/Triangle.py
@@ -1,8 +1,9 @@
-#Inverse pattern
+#Triangle pattern
 n=int(input("Enter number:"))
-for i in range(5,0,-1):
+for i in range(1,n+1):
     for j in range(n-i):
         print(" ",end="")
     for j in range(2*i-1):
         print("*",end="")
-    print() 
+    print()    
+        
